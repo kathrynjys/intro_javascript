1604dc8780449a4fd0e27b781cc0cedc9c027830
kathrynjys <42396042+kathrynjys@users.noreply.github.com>
2019-05-19 18:22:04 -0400

    Add files via upload

---
+++ js/invitation.js
@@ -0,0 +1,25 @@
+
+
+//function created to replace the content
+function replaceContent() {
+	//declaring the variables
+	var myRecipientName;
+    var myHostName; 
+	
+	//setting the variable to the input field's id named recipientNameInput's value
+    myRecipientName = document.getElementById("recipientNameInput").value;
+    
+    //setting the variable to the input field's id name hostNameInput value
+    myHostName = document.getElementById("hostNameInput").value; 
+   
+	console.log('Variable myRecipientName: ' + myRecipientName);
+    console.log('Variable myHostName: '+ myHostName);
+	
+	//setting the HTML code in the span id recipientNamePlaceholder with the variable 
+	document.getElementById("recipientNamePlaceholder").innerHTML = myRecipientName;
+    
+    //setting the HTML code in the span id hostNamePlaceholder with the variable
+    document.getElementById("hostNamePlaceholder").innerHTML = myHostName;
+    
+}
+    
